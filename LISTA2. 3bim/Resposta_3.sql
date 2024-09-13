b7788fc0c2eb9b3e8e83327f24ca9463a913a686
Nayarasc <142925177+Nayarasc@users.noreply.github.com>
2024-09-13 00:49:52 -0300

    Add files via upload

---
+++ LISTA2. 3bim/Resposta_3.sql
@@ -0,0 +1,5 @@
+USE db_financas_exam;
+SELECT nome, contas.tipo as "tipo_conta", saldo, data_criacao, transacoes.tipo as "tipo_transacao", valor, data_transacao, descricao
+FROM contas LEFT JOIN transacoes
+ON contas.conta_id = transacoes.conta_id
+;
